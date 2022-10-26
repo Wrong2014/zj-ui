1547aefc0b9cdf9c07785369a3fee8b9c1025c9c
wbh <1070750088@qq.com>
2022-10-26 19:16:37 +0800

    new

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,5 @@
 import {createApp} from "vue";
 import App from "./App.vue"
-import PUI from '../../src/index.js'
+import zjUI from '../../src/index.js'
 import '../../src/styles/index.less'
-createApp(App).use(PUI).mount('#app')
+createApp(App).use(zjUI).mount('#app')
